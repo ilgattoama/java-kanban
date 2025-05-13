1f3e8c75284fccf24b6f078523d6e3098da9711f
eungatto <evowvq@mail.ru>
2025-05-13 17:45:06 +0300

    Новые изменения

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -7,18 +7,18 @@ import java.util.*;
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int HISTORY_LIMIT = 10;
     private final LinkedList<Task> history = new LinkedList<>();
-    private final Set<Integer> historyIds = new HashSet<>();
 
     @Override
     public void add(Task task) {
         if (task == null) return;
+
         remove(task.getId());
+
         if (history.size() >= HISTORY_LIMIT) {
-            Task removed = history.removeFirst();
-            historyIds.remove(removed.getId());
+            history.removeFirst();
         }
+
         history.addLast(task);
-        historyIds.add(task.getId());
     }
 
     @Override
@@ -33,7 +33,6 @@ public class InMemoryHistoryManager implements HistoryManager {
             Task task = iterator.next();
             if (task.getId() == id) {
                 iterator.remove();
-                historyIds.remove(id);
                 break;
             }
         }
