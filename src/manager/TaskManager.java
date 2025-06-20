f72b41182a8ef8bbe434f3062ee71f7b10795258
eungatto <evowvq@mail.ru>
2025-06-20 14:48:41 +0300

    Внесены изменения для проверки 10

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -5,28 +5,41 @@ import task.*;
 import java.util.List;
 
 public interface TaskManager {
+
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     Task createTask(Task task);
+
     Epic createEpic(Epic epic);
+
     Subtask createSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTask(int id);
+
     void deleteEpic(int id);
+
     void deleteSubtask(int id);
 
     List<Subtask> getSubtasksByEpicId(int epicId);
