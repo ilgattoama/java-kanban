90b11f4dbf9cb80325a4683c5eb5e74de14cb2be
eungatto <evowvq@mail.ru>
2025-06-23 17:10:40 +0300

    Внесены изменения для проверки 32

--- src/task/Task.java
+++ src/task/Task.java
@@ -2,28 +2,54 @@ package task;
 
 import java.util.Objects;
 
-public class Task {
+public class Task
+{
     private int id;
     private String name;
     private String description;
     private Status status;
 
-    public Task(int id, String name, String description, Status status) {
+    public Task(int id, String name, String description, Status status)
+    {
         this.id = id;
         this.name = name;
         this.description = description;
         this.status = status;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    public Status getStatus() { return status; }
-    public void setStatus(Status status) { this.status = status; }
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public Status getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(Status status)
+    {
+        this.status = status;
+    }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (!(o instanceof Task)) return false;
         Task task = (Task) o;
@@ -31,7 +57,8 @@ public class Task {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hash(id);
     }
 }
