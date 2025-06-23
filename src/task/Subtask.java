6aaab48f6f62e2e15509ad1435f23c7e5965a315
eungatto <evowvq@mail.ru>
2025-06-23 17:20:18 +0300

    Внесены изменения для проверки 35

--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -2,24 +2,20 @@ package task;
 
 import java.util.Objects;
 
-public class Subtask extends Task
-{
+public class Subtask extends Task {
     private final int epicId;
 
-    public Subtask(int id, String name, String description, Status status, int epicId)
-    {
+    public Subtask(int id, String name, String description, Status status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
     }
 
-    public int getEpicId()
-    {
+    public int getEpicId() {
         return epicId;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Subtask)) return false;
         Subtask that = (Subtask) o;
@@ -27,8 +23,7 @@ public class Subtask extends Task
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return Objects.hash(getId());
     }
 }
