13deb8015ec5a59cbd6b98efbd0ccb98b4c77d2a
eungatto <evowvq@mail.ru>
2025-06-24 15:33:51 +0300

    Внесены изменения для проверки 39

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -1,61 +1,57 @@
-package manager;
+package test.manager;
 
-import org.junit.jupiter.api.*;
-import tasks.Task;
-import tasks.Status;
-
+import manager.HistoryManager;
+import manager.Managers;
+import task.Task;
+import task.Status;
+import org.junit.jupiter.api.Test;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
-    private HistoryManager historyManager;
-
-    @BeforeEach
-    void setUp() {
-        historyManager = new InMemoryHistoryManager();
-    }
 
     @Test
-    void addTaskToHistory() {
+    void add() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
         Task task = new Task(1, "Помыть посуду", "", Status.NEW);
         historyManager.add(task);
 
         List<Task> history = historyManager.getHistory();
-        assertEquals(1, history.size());
-        assertEquals(task, history.get(0));
+        assertNotNull(history, "История не пустая.");
+        assertEquals(1, history.size(), "История не пустая.");
     }
 
     @Test
-    void removeTaskFromHistory() {
+    void addDuplicate() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
         Task task1 = new Task(1, "Задача 1", "", Status.NEW);
-        Task task2 = new Task(2, "Задача 2", "", Status.NEW);
+        Task task2 = new Task(1, "Задача 1", "", Status.NEW);
+
+        historyManager.add(task1);
+        historyManager.add(task2);
+
+        List<Task> history = historyManager.getHistory();
+        assertEquals(1, history.size(), "Дубликат не добавился.");
+    }
+
+    @Test
+    void remove() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        Task task1 = new Task(1, "Первая", "", Status.NEW);
+        Task task2 = new Task(2, "Вторая", "", Status.NEW);
 
         historyManager.add(task1);
         historyManager.add(task2);
         historyManager.remove(1);
 
         List<Task> history = historyManager.getHistory();
-        assertEquals(1, history.size());
-        assertEquals(task2, history.get(0));
+        assertEquals(1, history.size(), "Задача не удалилась.");
+        assertEquals(2, history.get(0).getId(), "Удалена не та задача.");
     }
 
     @Test
-    void keepInsertionOrder() {
-        Task task1 = new Task(1, "Первая", "", Status.NEW);
-        Task task2 = new Task(2, "Вторая", "", Status.NEW);
-
-        historyManager.add(task1);
-        historyManager.add(task2);
-
-        List<Task> history = historyManager.getHistory();
-        assertEquals(2, history.size());
-        assertEquals(task1, history.get(0));
-        assertEquals(task2, history.get(1));
-    }
-
-    @Test
-    void replaceDuplicates() {
+    void updateTask() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
         Task task1 = new Task(1, "Старая версия", "", Status.NEW);
         Task task2 = new Task(1, "Новая версия", "", Status.DONE);
 
@@ -63,7 +59,7 @@ class InMemoryHistoryManagerTest {
         historyManager.add(task2);
 
         List<Task> history = historyManager.getHistory();
-        assertEquals(1, history.size());
-        assertEquals(task2, history.get(0));
+        assertEquals(1, history.size(), "Обновленная задача добавилась как новая.");
+        assertEquals("Новая версия", history.get(0).getName(), "Задача не обновилась.");
     }
 }
--- test/manager/InMemoryTaskManagerTest.java
+++ test/manager/InMemoryTaskManagerTest.java
@@ -1,68 +1,44 @@
-package manager;
+package test.manager;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import manager.TaskManager;
+import manager.Managers;
 import task.Task;
 import task.Epic;
 import task.Subtask;
 import task.Status;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest {
 
-    private TaskManager manager;
-
-    @BeforeEach
-    void setup() {
-        manager = Managers.getDefault();
-    }
-
     @Test
-    void createAndRetrieveTasksById() {
-        Task task = new Task(0, "Task", "Desc", Status.NEW);
+    void addAndFindTasks() {
+        TaskManager manager = Managers.getDefault();
+
+        Task task = new Task(0, "Задача", "Описание", Status.NEW);
         manager.createTask(task);
 
-        Epic epic = new Epic(0, "Epic", "Desc");
+        Epic epic = new Epic(0, "Эпик", "Описание");
         manager.createEpic(epic);
 
-        Subtask sub = new Subtask(0, "Sub", "Desc", Status.NEW, epic.getId());
+        Subtask sub = new Subtask(0, "Подзадача", "Описание", Status.NEW, epic.getId());
         manager.createSubtask(sub);
 
-        assertEquals(task, manager.getTaskById(task.getId()));
-        assertEquals(epic, manager.getEpicById(epic.getId()));
-        assertEquals(sub, manager.getSubtaskById(sub.getId()));
+        assertEquals(task, manager.getTask(task.getId()));
+        assertEquals(epic, manager.getEpic(epic.getId()));
+        assertEquals(sub, manager.getSubtask(sub.getId()));
     }
 
     @Test
-    void taskIdGenerationShouldNotConflictWithManualId() {
-        Task t1 = new Task(999, "Manual", "Desc", Status.NEW);
-        manager.createTask(t1);
+    void historySizeLimited() {
+        TaskManager manager = Managers.getDefault();
 
-        Task t2 = new Task(0, "Generated", "Desc", Status.NEW);
-        manager.createTask(t2);
+        for (int i = 0; i < 15; i++) {
+            Task task = new Task(i, "Задача " + i, "", Status.NEW);
+            manager.createTask(task);
+            manager.getTask(task.getId());
+        }
 
-        assertNotEquals(t1.getId(), t2.getId());
-    }
-
-    @Test
-    void addedTaskShouldRemainUnchanged() {
-        Task task = new Task(0, "Immutable", "Test", Status.NEW);
-        manager.createTask(task);
-
-        assertEquals("Immutable", task.getName());
-        assertEquals("Test", task.getDescription());
-        assertEquals(Status.NEW, task.getStatus());
-    }
-
-    @Test
-    void subtaskShouldNotHaveSameIdAsEpic() {
-        Epic epic = new Epic(0, "Epic", "Desc");
-        manager.createEpic(epic);
-
-        Subtask subtask = new Subtask(0, "Sub", "Desc", Status.NEW, epic.getId());
-        manager.createSubtask(subtask);
-
-        assertNotEquals(subtask.getId(), epic.getId());
+        assertEquals(10, manager.getHistory().size(), "История должна ограничиваться 10 элементами");
     }
 }
