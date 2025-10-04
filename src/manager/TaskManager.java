785f99dcd198956705a59445817cf06bd25bc940
eungatto <evowvq@mail.ru>
2025-10-04 16:08:37 +0300

    Добавление пустых строк в TaskManager

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,27 +1,36 @@
 package manager;
 
 import task.*;
-import java.util.List;
 
 public interface TaskManager {
-    List<Task> getAllTasks();
-    List<Epic> getAllEpics();
-    List<Subtask> getAllSubtasks();
-    void deleteAllTasks();
-    void deleteAllEpics();
-    void deleteAllSubtasks();
-    Task getTaskById(int id);
-    Epic getEpicById(int id);
-    Subtask getSubtaskById(int id);
+
     Task createTask(Task task);
+
     Epic createEpic(Epic epic);
+
     Subtask createSubtask(Subtask subtask);
+
+    Task getTaskById(int id);
+
+    Epic getEpicById(int id);
+
+    Subtask getSubtaskById(int id);
+
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
-    void deleteTask(int id);
-    void deleteEpic(int id);
-    void deleteSubtask(int id);
-    List<Subtask> getSubtasksByEpicId(int epicId);
-    List<Task> getHistory();
+
+    void deleteTaskById(int id);
+
+    void deleteEpicById(int id);
+
+    void deleteSubtaskById(int id);
+
+    void deleteAllTasks();
+
+    void deleteAllEpics();
+
+    void deleteAllSubtasks();
 }
